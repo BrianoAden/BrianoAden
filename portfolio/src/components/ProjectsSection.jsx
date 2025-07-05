b276580ebfe5129ca37d467253bf87af37d6beac
Aden Briano <brianoeaden@gmail.com>
2025-07-04 18:01:21 -0700

    fixed projects

--- portfolio/src/components/ProjectsSection.jsx
+++ portfolio/src/components/ProjectsSection.jsx
@@ -7,7 +7,7 @@ const projects = [
         title: "SnakeASIC",
         description: "ASIC developed in Verilog using Controller/Datapath architecture to simulate the class Snake game!",
         image: "projects/integrated_core.png",
-        tags: ["Magic", "Verilog"],
+        tags: ["Magic", "Verilog", "Questasim", "Irsim", "Innovus P&R"],
         demoUrl: "assets/ELEC_422_Final_Report.pdf",
         githubUrl: "https://github.com/BrianoAden/SnakeASIC"  
     },
@@ -16,7 +16,7 @@ const projects = [
         title: "Adder Design Competition",
         description: "Design competition where I implemented a Full Adder and Multiplexer in various logic families to determine the design that yielded the smallest propagation delay for each device!",
         image: "projects/Kogge_Stone.png",
-        tags: [],
+        tags: ["Cadence Virtuoso"],
         demoUrl: "assets/ELEC423_Final_Report.pdf",
         githubUrl: "#"  
     },
@@ -25,7 +25,7 @@ const projects = [
         title: "32b-RISCV",
         description: "My current project! I am working on implementing a full 32b-RISC-V soft processor in Verilog, using Questasim to verify functionality.",
         image: "projects/riscv.jpeg",
-        tags: [],
+        tags: ["Magic", "Verilog", "Questasim", "Irsim", "Innovus P&R", "Computer Architecture", "RISC-V"],
         demoUrl: "#",
         githubUrl: "https://github.com/BrianoAden/32bRISC-V"  
     }
@@ -44,28 +44,27 @@ export const ProjectsSection = () => {
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {projects.map((project, key) => (
-                    <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
+                    <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover relative">
                         <div className="h-48 overflow-hidden">
                             <img src={project.image} alt={project.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"/>
                         </div>
 
-                        <div className="p-6">
+                        <div className="p-4 mb-10">
                             <div className="flex flex-wrap gap-2 mb-4">
                                 {project.tags.map((tag) => (
-                                    <span className="px-2 py-1 text-xs font-medium rounded-full bg-secondary text-secondary-foreground">
+                                    <span className="px-2 py-1 text-xs font-medium rounded-full bg-primary/20 text-secondary-foreground">
                                         {tag}
                                     </span>
                                 ))}
                             </div>
-
-                        <h3 className=""text-xl font-semibold mb-1> {project.title}</h3>
+                            <div className="">
+                        <h3 className="text-xl font-semibold mb-1"> {project.title}</h3>
                         <p className="text-muted-foreground text-sm mb-4"> {project.description} </p>
-                        <div className="flex justify-between items-center">
-                            <div className="flex space-x-3">
-                                <a href = {project.demoUrl} target = "_blank" className="text-foreground/80 hover:text-primary transition-colors duration-300"> <ExternalLink size={20}/> </a>
-                                <a href = {project.githubUrl} target = "_blank" className="text-foreground/80 hover:text-primary transition-colors duration-300"> <Github size={20}/> </a>
-                            </div>
                         </div>
+                            <div className="flex space-x-3 absolute bottom-0 left-5 size-12">
+                                <a href = {project.demoUrl} target = "_blank" className="text-foreground/80 hover:text-primary transition-colors duration-300 bottom"> <ExternalLink size={20}/> </a>
+                                <a href = {project.githubUrl} target = "_blank" className="text-foreground/80 hover:text-primary transition-colors duration-300 bottom"> <Github size={20}/> </a>
+                            </div>
                         </div>
                     </div>
             ))}
