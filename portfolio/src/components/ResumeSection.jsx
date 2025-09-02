0577901393787f09914c666132b101c1b0dc7173
Aden Briano <brianoeaden@gmail.com>
2025-09-01 20:38:08 -0500

    added website logo and changed website title

--- portfolio/src/components/ResumeSection.jsx
+++ portfolio/src/components/ResumeSection.jsx
@@ -1,8 +1,8 @@
 export const ResumeSection = () => {
     return (
     <section id="resume" className = "py-24 px-4 relative">
-        <div className="h-170">
-            <iframe className="w-full h-200" src = "assets/BrianoAden_Resume2025.pdf" frameborder = "0"></iframe>
+        <div className="md:h-170 sm:h-150">
+            <iframe className="w-full md:h-200 sm:h-150" src = "assets/BrianoAden_Resume2025.pdf" frameborder = "0"></iframe>
         </div>
     </section>
     );
